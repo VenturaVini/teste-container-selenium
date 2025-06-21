f3c0b0e98e8b48aded8185d45e81fc37dec8d600
VenturaVini <110046348+VenturaVini@users.noreply.github.com>
2025-06-21 09:49:58 +0000

    ok telegram

--- teste.py
+++ teste.py
@@ -1,4 +1,5 @@
 from selenium_client import SeleniumClient
+from services.telegram import enviar_mensagem
 
 def main():
     url = 'https://www.youtube.com/watch?v=zuYWJ2AVvPw'
@@ -13,7 +14,8 @@ def main():
         # 👇 Aqui você faz seu scraping como quiser
         print(f"Título: {driver.title}")
         h1 = driver.find_element("tag name", "h1")
-        print(f"<h1>: {h1.text}")
+        print(f"Texto Extraido: {h1.text}")
+        enviar_mensagem(h1.text)
     except Exception as e:
         print(f"⚠️ Erro durante scraping: {str(e)}")
     finally:
